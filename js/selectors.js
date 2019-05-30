020e53edc800fb5361ed78581bafeab1090a9514
Arye Eidelman <aryedov.e@gmail.com>
2019-05-30 17:01:51 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+
+}
+
+function ninjaBabySelector() {
+
+}
+
+function divSelector() {
+
+}
+
+function firstListItem() {
+
+}
